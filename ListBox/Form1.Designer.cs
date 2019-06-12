e763a8962d67a53727ef0f4cfd873577ccf2ce84
Jinju Kim <swinging13@naver.com>
2019-06-12 11:21:44 +0900

    #2 코드구현

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -43,6 +43,7 @@
             this.lbView.Name = "lbView";
             this.lbView.Size = new System.Drawing.Size(278, 136);
             this.lbView.TabIndex = 0;
+            this.lbView.SelectedIndexChanged += new System.EventHandler(this.LbView_SelectedIndexChanged);
             // 
             // lblResult
             // 
@@ -59,6 +60,7 @@
             this.txtList.Name = "txtList";
             this.txtList.Size = new System.Drawing.Size(278, 21);
             this.txtList.TabIndex = 2;
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtList_KeyPress);
             // 
             // btnAdd
             // 
@@ -68,6 +70,7 @@
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // Form1
             // 
@@ -82,6 +85,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "리스트 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
